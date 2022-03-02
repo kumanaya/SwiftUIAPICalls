53f17b1baca5fab3f26db9176c1df323dbcc300a
Worcoach <worcoach@gmail.com>
2022-03-02 20:48:40 -0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Daniel Kumanaya on 02/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SwiftUIAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallsApp.swift
+//  Shared
+//
+//  Created by Daniel Kumanaya on 02/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
